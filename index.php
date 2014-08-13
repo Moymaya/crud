6b0f59037dabaa15b8578b9216ab20124cf8e3dc
Moymaya <hunab_kum@hotmail.com>
2014-08-12 20:31:17 -0500

    Agregar menu.html

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+Pagina principal del sistema crud de productos para iFactory
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>iFrame</title>
+    </head>
+    <body>
+    <center>
+        <?php
+        // put your code here
+        echo"<h2>Sistema CRUD Productos</h2>";
+        echo"<br>";
+        //incluimos el archivo menu.html que contiene el menu de la aplicacion
+        include "menu.html";
+        ?>
+    </center>  
+    </body>
+</html>
